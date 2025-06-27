d7a6157e28fd8ee8f05be66eaa2bd5feb8d0bc80
kimain050401 <kimain0401@gmail.com>
2025-06-27 23:01:59 +0900

    Initial Commit

---
+++ rua/ruaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ruaApp.swift
+//  rua
+//
+//  Created by 김아인 on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ruaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
