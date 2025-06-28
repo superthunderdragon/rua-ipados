8df6565dac1d1f656d574a9158186764e8b6b6eb
kimain050401 <kimain0401@gmail.com>
2025-06-28 10:00:16 +0900

    :tada: New Student Pages Design to Code

---
+++ rua/Views/LearningView.swift
@@ -0,0 +1,18 @@
+//
+//  LearningView.swift
+//  rua
+//
+//  Created by 김아인 on 6/28/25.
+//
+
+import SwiftUI
+
+struct LearningView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearningView()
+}
